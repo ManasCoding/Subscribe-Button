a65132d8c02a24053b9ba553151b356a711a9525
manaskumar <manaskumar82499@gmail.com>
2024-04-02 14:18:06 +0530

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+// To access the show button element 
+let showbtn = document.getElementById("showbtn"); 
+
+// To access the Close button element 
+let closebtn = document.getElementById("closebtn"); 
+
+// To acces the popup element 
+let popup = document.querySelector(".popup"); 
+let subp = document.getElementById("sub-p"); 
+
+// To show the popup on click 
+showbtn.addEventListener("click",() => { 
+	popup.style.display = "block"; 
+	showbtn.style.display = "none"; 
+	subp.style.display = "none"; 
+}); 
+
+// To close the popup on click 
+closebtn.addEventListener("click",() => { 
+	popup.style.display = "none"; 
+	showbtn.style.display = "block";  
+	subp.style.display = "block"; 
+});
